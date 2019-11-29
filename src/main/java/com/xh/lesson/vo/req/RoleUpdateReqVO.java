cb84ab219b0f24833f271bf009aac82a65a71f6f
as <1327699142@qq.com>
2019-11-29 16:12:37 +0800

    用户juese

--- src/main/java/com/xh/lesson/vo/req/RoleUpdateReqVO.java
+++ src/main/java/com/xh/lesson/vo/req/RoleUpdateReqVO.java
@@ -9,10 +9,10 @@ import java.util.List;
 /**
  * @ClassName: RoleUpdateReqVO
  * TODO:类文件简单描述
- * @Author: 小霍
- * @CreateDate: 2019/9/20 0:29
- * @UpdateUser: 小霍
- * @UpdateDate: 2019/9/20 0:29
+ * @Author: as
+ * @CreateDate: 2019/10/20 0:29
+ * @UpdateUser: as
+ * @UpdateDate: 2019/10/20 0:29
  * @Version: 0.0.1
  */
 @Data

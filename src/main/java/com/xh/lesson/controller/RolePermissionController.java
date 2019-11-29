8dea0ea9f89042f78fc066dfd8a87e0b6ddf71c4
as <1327699142@qq.com>
2019-11-29 15:16:42 +0800

    角色和菜单控制层

--- src/main/java/com/xh/lesson/controller/RolePermissionController.java
+++ src/main/java/com/xh/lesson/controller/RolePermissionController.java
@@ -17,9 +17,9 @@ import javax.validation.Valid;
 /**
  * @ClassName: RolePermissionController
  * TODO:类文件简单描述
- * @Author: 小霍
+ * @Author: as
  * @CreateDate: 2019/9/19 11:37
- * @UpdateUser: 小霍
+ * @UpdateUser: as
  * @UpdateDate: 2019/9/19 11:37
  * @Version: 0.0.1
  */

6cf96f6bcf4ef7fce050973c9153f9da39687855
as <1327699142@qq.com>
2019-11-29 15:25:03 +0800

    返回编码接口

--- src/main/java/com/xh/lesson/exception/code/ResponseCodeInterface.java
+++ src/main/java/com/xh/lesson/exception/code/ResponseCodeInterface.java
@@ -2,11 +2,11 @@ package com.xh.lesson.exception.code;
 
 /**
  * @ClassName: ResponseCodeInterface
- * TODO:类文件简单描述
- * @Author: 小霍
- * @CreateDate: 2019/9/4 22:56
- * @UpdateUser: 小霍
- * @UpdateDate: 2019/9/4 22:56
+ * TODO: 编码接口
+ * @Author: as
+ * @CreateDate: 2019/10/4 22:56
+ * @UpdateUser: as
+ * @UpdateDate: 2019/10/4 22:56
  * @Version: 0.0.1
  */
 public interface ResponseCodeInterface {

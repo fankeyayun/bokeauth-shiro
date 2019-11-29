c2e0b5cc672194c87291db5b681d6f4138e0eec6
as <1327699142@qq.com>
2019-11-29 15:57:38 +0800

    first commit

--- src/main/java/com/xh/lesson/shiro/CustomHashedCredentialsMatcher.java
+++ src/main/java/com/xh/lesson/shiro/CustomHashedCredentialsMatcher.java
@@ -14,10 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 /**
  * @ClassName: CustomHashedCredentialsMatcher
  * TODO:类文件简单描述
- * @Author: 小霍
- * @CreateDate: 2019/9/7 13:42
- * @UpdateUser: 小霍
- * @UpdateDate: 2019/9/7 13:42
+ * @Author: as
+ * @CreateDate: 2019/10/7 13:42
+ * @UpdateUser: as
+ * @UpdateDate: 2019/10/7 13:42
  * @Version: 0.0.1
  */
 public class CustomHashedCredentialsMatcher extends SimpleCredentialsMatcher {

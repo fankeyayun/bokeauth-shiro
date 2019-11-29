3143855dda24ed3d668f314e88a660ef0cff15d1
as <1327699142@qq.com>
2019-11-29 15:32:45 +0800

    系统日志服务

--- src/main/java/com/xh/lesson/service/LogService.java
+++ src/main/java/com/xh/lesson/service/LogService.java
@@ -7,19 +7,19 @@ import com.xh.lesson.vo.resp.PageVO;
 /**
  * @ClassName: LogService
  * TODO:类文件简单描述
- * @Author: 小霍
- * @CreateDate: 2019/9/23 16:17
- * @UpdateUser: 小霍
- * @UpdateDate: 2019/9/23 16:17
+ * @Author: as
+ * @CreateDate: 2019/10/23 16:17
+ * @UpdateUser: as
+ * @UpdateDate: 2019/10/23 16:17
  * @Version: 0.0.1
  */
 public interface LogService {
     /**
      * 分页获取系统操作日志
-     * @Author:      小霍
-     * @CreateDate:  2019/9/23 16:21
+     * @Author:      as
+     * @CreateDate:  2019/10/23 16:21
      * @UpdateUser:
-     * @UpdateDate:  2019/9/23 16:21
+     * @UpdateDate:  2019/10/23 16:21
      * @Version:     0.0.1
      * @param vo
      * @return       com.xh.lesson.vo.resp.PageVO<com.xh.lesson.entity.SysLog>
